d2faaa5040779cc5f721db7fe83fddad50ece79e
EstebanXDQ <165889880+EstebanGM1DAW@users.noreply.github.com>
2024-10-29 10:07:23 +0100

    Add files via upload

---
+++ Relacion5_1.java
@@ -0,0 +1,48 @@
+public class Relacion5_1 {
+    public static void main(String[] args) {
+        int p = 1;
+        int q = 2;
+        float x = 1.1f;
+        float y = 1.2f;
+        float z = 1.3f;
+        char a = 'x';
+        char b = 'x';
+        char c = 'x';
+        double raiz1 = 1.1d;
+        double raiz2 = 1.2d;
+        long contador = 90000;
+        short indicador = 900;
+        int indice = 90;
+        double precio = 10.1d;
+        double precioFinal = 10.2d;
+        char car1 = '1';
+        char car2 = '2';
+        byte valor = 1;
+        boolean primero = true;
+        boolean ultimo = true;
+        String nombre = "jose";
+
+        System.out.println(p);
+        System.out.println(q);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(raiz1);
+        System.out.println(raiz2);
+        System.out.println(contador);
+        System.out.println(indicador);
+        System.out.println(indice);
+        System.out.println(precio);
+        System.out.println(precioFinal);
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(valor);
+        System.out.println(primero);
+        System.out.println(ultimo);
+        System.out.println(nombre);
+
+    }
+}
