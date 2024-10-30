85bcbfcc6950d5ddac0c9e8ba5e9677913cf95cc
EstebanXDQ <165889880+EstebanGM1DAW@users.noreply.github.com>
2024-10-30 14:12:12 +0100

    Add files via upload

---
+++ Relacion5_2.java
@@ -0,0 +1,49 @@
+public class Relacion5_2 {
+    public class VariablesEjemplo {
+        public static void main(String[] args) {
+            byte edad = 25;
+            int codigoPostal = 28001;
+            float altura = 1.75f;
+            char genero = 'H';
+            String nombre = "Juan";
+            int numeroDeHijos = 2;
+            double iva = 21.0;
+            int tallaCamisa = 42;
+            float peso = 70.5f;
+            double precio = 99.99;
+            boolean alumnoRepetidor = false;
+            String mensaje = "Bienvenido a Java";
+            char letra = 'A';
+            int mayorEdad = 18;
+            int minutos = 120;
+            int dias = 365;
+            String matriculaCoche = "1234ABC";
+            int contador = 10;
+            boolean mayorDeEdad = true;
+            String tallaCamiseta = "L";
+    
+            System.out.println("Edad: " + edad + 
+                               ", Código Postal: " + codigoPostal +
+                               ", Altura: " + altura +
+                               ", Género: " + genero +
+                               ", Nombre: " + nombre +
+                               ", Número de Hijos: " + numeroDeHijos +
+                               ", IVA: " + iva + "%" +
+                               ", Talla Camisa: " + tallaCamisa +
+                               ", Peso: " + peso +
+                               ", Precio: " + precio +
+                               ", Alumno Repetidor: " + alumnoRepetidor +
+                               ", Mensaje: " + mensaje +
+                               ", Letra: " + letra +
+                               ", Mayor de Edad: " + mayorEdad +
+                               ", Minutos: " + minutos +
+                               ", Días: " + dias +
+                               ", Matrícula Coche: " + matriculaCoche +
+                               ", Contador: " + contador +
+                               ", Mayor de Edad (boolean): " + mayorDeEdad +
+                               ", Talla Camiseta: " + tallaCamiseta);
+        }
+    }
+    
+}
+
---
+++ Relacion5_3.java
@@ -0,0 +1,44 @@
+public class Relacion5_3 {
+    public static void main(String[] args) {
+        double x,y,a,b,c,d,e,f,g,h,j,m,n,p,q,r,s;
+        x = 1.0d;
+        y = 1.0d;
+        a = 1.0d;
+        b = 1.0d;
+        c = 1.0d;
+        d = 1.0d;
+        e = 1.0d;
+        f = 1.0d;
+        g = 1.0d;
+        h = 1.0d;
+        j = 1.0d;
+        m = 1.0d;
+        n = 1.0d;
+        p = 1.0d;
+        q = 1.0d;
+        r = 1.0d;
+        s = 1.0d;
+        double ejeA = 3.0/2.0 + 4.0/3.0;
+        System.out.println(ejeA);
+        double ejeB = (1.0/x-5.0)-(3.0*x*y/4.0);
+        System.out.println(ejeB);
+        double ejeC = 1.0/2.0+7.0;
+        System.out.println(ejeC);
+        double ejeD = 7.0 + (1.0/2.0);
+        System.out.println(ejeD);
+        double ejeE = ((a*a)/b-c) + (d-e) / (f-((g*h)/j));
+        System.out.println(ejeE);
+        double ejeF = m/n + p;
+        System.out.println(ejeF);
+        double ejeG = m + (n/(p-q));
+        System.out.println(ejeG);
+        double ejeH = (a*a)/(b*b)+(c*c)/(d*d);
+        System.out.println(ejeH);
+        double ejeI = (m + n/p)/(q - r/s);
+        System.out.println(ejeI);
+        double ejeJ = (3.0*a + b)/((c - (d+5.0/e))/(f + g/(2.0*h)));
+        System.out.println(ejeJ);
+        double ejeK = ((a*a) + (2.0*a*b) + (b*b))/(((1.0*x)/2.0)+2.0);
+        System.out.println(ejeK);
+    }
+}
